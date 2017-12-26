31ce0be5670f2c39b7599ea9767fa8aed813efd0
AleksandarColovic <AleksandarColovic@users.noreply.github.com>
2017-12-26 01:18:01 +0100

    Update ColorGame.js

--- ColorGame.js
+++ ColorGame.js
@@ -10,14 +10,14 @@ var hard = document.querySelector("#hard");
 
 //LOCAL VARS
 
-var difficulty;
+var difficulty = 6;
 var colors = arrGen(difficulty);
 var pickedColor = pickColor();
 var b = false;
 
 // FUNCTIONS
 
-// random number between 0 and 255
+// generates a random number between 0 and 255
 function randomizer(){
 	return Math.floor(Math.random() * 256);
 }
@@ -26,91 +26,97 @@ function rgbGenerator(){
 	return "rgb(" + randomizer() + ", " + randomizer() + ", " + randomizer() + ")";
 }
 
-// generates an array of rgb colors with the size defined by 
-
-function arrGen(num){
+// generates an array of rgb colors with the length defined by diff
+function arrGen(diff){
 	var arr = [];
-	for (i = 0; i < num; i++){
+	for (i = 0; i < diff; i++){
 		arr.push(rgbGenerator());
 	}
 	return arr;
 }
 
-
 // randomly picks one color from the array of colors mentioned above
 function pickColor(){
-	var rand = Math.floor(Math.random() * difficulty);
-	return colors[rand];
+	return colors[(Math.floor(Math.random() * difficulty))]
 }
 
-// changes the background of the difficulty number of squares
+// changes the color of all squares into one color
 function changeColors(color){
-
-	for(i = 0; i < difficulty; i++){
+	for(i = 0; i < 6; i++){
 		squares[i].style.backgroundColor = color;
 	}
 }
 
-function resetG(){
-	reset.textContent = "New Colors";
-	message.textContent = "";
-	h1.style.backgroundColor = "steelblue";
-	
-	console.log("First console log: " + difficulty);  // <======================================= first console log
-	if(difficulty == 3){
+// switches the amount of displayed squares depending on the diff
+function displaySwitching(diff){
+	if(diff === 3){
 		for(i = 3; i < 6; i++){
-			console.log("I'm in the first loop! Yaaaay!");
 			squares[i].style.display = "none";
+			return;
 		}
-	}else{
-		for(i = 3; i < 6; i++){
-			console.log("I'm in the second loop! Fuuuuuck!");
-			squares[i].style.display = "inline-block";
+		if(diff === 6){
+			for(i = 3; i < 6; i++){
+				squares[i].style.display = "inline-block";
+				return;
+			}
 		}
+		console.log("DISPLAY SWITCHING FAILED!!!");
 	}
 
-	colors = arrGen(difficulty);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor.toUpperCase();
+// first colors all squares with random colors, then handles the clicking after 
+function clickedColor(){
 	for(i = 0; i < squares.length; i++){
-		console.log("second console log: " + difficulty);             // <====================================== second console log
 		squares[i].style.backgroundColor = colors[i];
-		squares[i].addEventListener("click", function(){
-			var clickedColor = this.style.backgroundColor;
-			if(clickedColor === pickedColor){
-				message.textContent = "Correct!";
-				changeColors(clickedColor);
-				h1.style.backgroundColor = clickedColor;
-				reset.textContent = "Try Again?";
-			}else{
-				this.style.backgroundColor = "#232323";
-				message.textContent = "Try Again.";
-			}
+		squares[i].addEventListener("click", 
+			function(){
+				var clickedColor = this.style.backgroundColor;
+				if(clickedColor === pickedColor){
+					message.textContent = "Correct!";
+					changeColors(clickedColor);
+					h1.style.backgroundColor = clickedColor;
+					reset.textContent = "Try Again?";
+				}else{
+					this.style.backgroundColor = "#232323";
+					message.textContent = "Try Again.";
+				}
 
-		});
+			});
 	}
 }
 
-//EXECUTION
+// resets the squares
+function resetGame(){
+	reset.textContent = "New Colors";
+	message.textContent = "";
+	h1.style.backgroundColor = "steelblue";
+	displaySwitching(difficulty);
+	colors = arrGen(difficulty);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor.toUpperCase();
+	clickedColor();
+}
 
-resetG();
+
+// EXECUTION AND LISTENERS
+
+resetGame();
 hard.classList.add("selected");
 
 
 reset.addEventListener("click", function(){
-	resetG();
+	resetGame();
 });
 
 easy.addEventListener("click", function(){
 	difficulty = 3;
-	resetG();
+	resetGame();
 	hard.classList.remove("selected");
 	easy.classList.add("selected");
 });
 
 hard.addEventListener("click", function(){
 	difficulty = 6;
-	resetG();
+	resetGame();
 	easy.classList.remove("selected");
 	hard.classList.add("selected");
 });
